9a244f7bf60e24fc1c4d787ba07dc33f2c57ac37
Anna <anya@tower.lessmore.pw>
2023-01-25 03:35:52 +0300

    Добавлено взаимодействие с sentry

--- ugc/src/app/app.py
+++ ugc/src/app/app.py
@@ -1,18 +1,22 @@
+import sentry_sdk
 from flask import Blueprint, Flask, request
-
 from src.api.v1.movies import movies
 from src.api.v1.openapi import openapi
-
+from src.configs.sentry import sentry_config
 from src.services.auth import init_auth
-from src.services.streamer import init_streamer
-from src.services.storage import init_storage
 from src.services.logger import init_logger
+from src.services.storage import init_storage
+from src.services.streamer import init_streamer
 from src.utils.encoders import JSONEncoder
-
 
 app = Flask(__name__)
 app.json_encoder = JSONEncoder
 app.config['PROPAGATE_EXCEPTIONS'] = True
+
+sentry_sdk.init(
+    dsn=sentry_config.dsn,
+    traces_sample_rate=sentry_config.traces_sample_rate
+)
 
 init_auth(app)
 init_streamer(app)
---
+++ ugc/src/configs/sentry.py
@@ -0,0 +1,12 @@
+from pydantic import Field, Required
+from src.configs.base import BaseConfig
+
+
+class SentryConfig(BaseConfig):
+    """Настройки Sentry."""
+
+    dsn: str = Field(Required, env="SENTRY_DSN")
+    traces_sample_rate: float = Field(1.0, env="SENTRY_TRACES_SAMPLE_RATE")
+
+
+sentry_config = SentryConfig()
