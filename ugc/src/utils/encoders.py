7b405b8421846711389a6f4a486bcf139c386349
mikhail349 <mikhail349349@gmail.com>
2023-01-25 11:28:47 +0300

    Update encoders.py

--- ugc/src/utils/encoders.py
+++ ugc/src/utils/encoders.py
@@ -5,6 +5,10 @@ from flask.json import JSONEncoder as BaseJSONEncoder
 
 
 class JSONEncoder(BaseJSONEncoder):
+    """Класс кодирования JSON, который приводит:
+    - `ObjectId` к `str`
+    - `datetime и date` к `ISO` формату
+    """
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
