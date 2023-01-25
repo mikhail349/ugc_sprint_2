c157bad71b496f423102ebb09442a8dad6ab43ab
Anna <sudnitsina@gmail.com>
2023-01-26 02:28:35 +0300

    Fix mypy issues

--- ugc/src/models/review.py
+++ ugc/src/models/review.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Optional
 import datetime
 import uuid
 
@@ -7,8 +7,8 @@ from pydantic import BaseModel, Field
 
 class MovieRating(BaseModel):
     """Модель рейтингов фильма."""
-    creator: int = None
-    overall: float = None
+    creator: Optional[int] = None
+    overall: Optional[float] = None
 
 
 class ReviewRating(BaseModel):
